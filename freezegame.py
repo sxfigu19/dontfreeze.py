b20b61535e3bdf1ebe597c258970d84767a89c38
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-02-25 14:34:08 -0500

    Update freezegame.py

--- freezegame.py
+++ freezegame.py
@@ -1,11 +1,10 @@
 import pyglet
 
-win = pyglet.window.Window()
+win = pyglet.window.Window() # place fullscreen=True inside ()
 
 water= pyglet.image.load('images/assets/ground/graphics-tiles-waterflow.png')
 spr1 = pyglet.sprite.Sprite(water, x=0, y =0)
 spr1.scale = 4
-
 land= pyglet.image.load('images/assets/ground/snow_stones.png')
 spr2 = pyglet.sprite.Sprite(land, x=135, y =55)
 spr2.scale = 1.5
@@ -13,7 +12,6 @@ spr2.scale = 1.5
 talltree= pyglet.image.load('images/assets/ground/tall_snow_tree.png')
 spr3 = pyglet.sprite.Sprite(talltree, x=380, y =310)
 spr3.scale = 1
-
 shorttree= pyglet.image.load('images/assets/ground/short_snow_tree.png')
 spr4 = pyglet.sprite.Sprite(shorttree, x=160, y =95)
 spr4.scale = 0.8
@@ -37,19 +35,19 @@ def update(dt):
     win.push_handlers(keys) # update the key object
     spr6.image = player
     if keys[pyglet.window.key.UP]:
-        print("Up key pressed!")
+        print("Moving Up")
         spr6.image = playerup
         spr6.y+=2
     if keys[pyglet.window.key.DOWN]:
-        print("Down key pressed!")
+        print("Moving Down")
         spr6.image = playerdown
         spr6.y-=2
     if keys[pyglet.window.key.LEFT]:
-        print("Left key pressed!")
+        print("Moving Left")
         spr6.image = playerleft
         spr6.x-=2
     if keys[pyglet.window.key.RIGHT]:
-        print("Right key pressed!")
+        print("Moving Right")
         spr6.image = playerright
         spr6.x+=2
 
